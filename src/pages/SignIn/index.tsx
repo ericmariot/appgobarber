7c1e42a288f16b1072160af6f388218df2674c80
Eric Mariot <ericmariots@gmail.com>
2020-09-15 16:27:34 -0300

    Implement Authentication and Register pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,27 +1,61 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useCallback } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Make your login</Title>
+    <>
+      <KeyboardAvoidingView 
+        style={{ flex: 1 }} 
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined} 
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
+            <View>
+              <Title>Make your login</Title>
+            </View>
+    
+            <Form onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="password" icon="lock" placeholder="Password" />
+              <Input name="password" icon="lock" placeholder="Password" />
 
-      <Button onPress={() => { console.log('Deu') }} >Entrar</Button>
-    </Container>
+              <Button onPress={() => { console.log('Deu') }} >Login</Button>
+            </Form>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Forgot my password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')} >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create an account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
+  
 }
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container />
+    <>
+      <KeyboardAvoidingView 
+        style={{ flex: 1 }} 
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined} 
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Create your account</Title>
+            </View>
+    
+            <Input name="name" icon="user" placeholder="Name" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button onPress={() => { console.log('Deu') }}>Register</Button>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.navigate('SignIn')} >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Back to login</BackToSignInText>
+      </BackToSignIn>
+    </>
   );
+  
 }
 
 export default SignUp;
