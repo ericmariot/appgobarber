48f58be754db2e4244dd8c70a3eed322df887d93
Eric Mariot <ericmariots@gmail.com>
2020-09-15 14:36:16 -0300

    first commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Make your login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button onPress={() => { console.log('Deu') }} >Entrar</Button>
+    </Container>
+  );
+}
+
+export default SignIn;
+
